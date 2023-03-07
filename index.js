2d599a03a8002f2d1af12f723e477be7ecde4c0a
Sina <alisinashafieezada@gmail.com>
2023-03-07 16:41:11 +0330

    web server access

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = ['http://example.com', 'http://localhost:3000'];
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 app.get('/getToken', async function (req, res) {
   try {
